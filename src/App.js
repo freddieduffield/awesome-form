04c069b38924b7505c673cf9089c5a52ca3c887f
FrederickFoxxx <fwhduffield@gmail.com>
2019-01-03 08:20:31 +0000

    added initial values

--- src/App.js
+++ src/App.js
@@ -6,8 +6,20 @@ class RegisterFormContainer extends Component {
     window.alert(JSON.stringify(values, null, 4));
   };
 
+  getInitialValues() {
+    return {
+      preference: 'spaces',
+      newsletter: true
+    };
+  }
+
   render() {
-    return <RegisterForm onSubmit={this.submit} />;
+    return (
+      <RegisterForm
+        onSubmit={this.submit}
+        initialValues={this.getInitialValues()}
+      />
+    );
   }
 }
 
